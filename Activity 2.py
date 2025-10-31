3cf4b0dadcade44880c1043f6d8cfadfb19bd874
Hiyaa2013 <hiyaah.shimpi@gmail.com>
2025-10-31 19:46:48 +0530

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,11 @@
+a=10
+b=-10
+#print bitwise right shift operator
+print("a >> 1=", a >> 1)
+print("a >> 2=", b >> 2)
+
+a=5
+b=-10
+#print bitwise left shift operator
+print("a << 1=", a << 1)
+print("b << 2=", b << 2)
