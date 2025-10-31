5a9ddecb61d8f7522bd1881a82ee80cec9a7a57d
Hiyaa2013 <hiyaah.shimpi@gmail.com>
2025-10-31 20:07:09 +0530

    Add files via upload

--- Activity 3.py
+++ Activity 3.py
@@ -1,19 +1,36 @@
-height = float(input("Enter your height in cm: "))
-weight = float(input("Enter your weight in kg: "))
+print("Enter  marks obtained in 5 subjects....")
+marks1=int(input())
+marks2=int(input())
+marks3=int(input())
+marks4=int(input())
+marks5=int(input())
 
-BMI = weight / (height/100)**2
+tot=marks1+marks2+marks3+marks4+marks5
+avg=tot/5 
 
-print("Your BMI is", BMI)
-
-if BMI <= 18.4:
-    print("You are underweight.")
-elif BMI <= 24.9:
-    print("You are healthy.")
-elif BMI <= 29.9:
-    print("You are over weight.")
-elif BMI <= 34.9:
-    print("You are severely over weight.")
-elif BMI <= 39.9:
-    print("You are obese.")
+if avg>=91 and avg<=100:
+    print("your grade is O+")
+elif avg>=81 and avg<=91:
+    print("your grade is A++")
+elif avg>=71 and avg<=81:
+    print("your grade  A+")
+elif avg>=61 and avg<=71:
+    print("your grade  A")
+elif avg>=51 and avg<=61:
+    print("your grade  B+")
+elif avg>=41 and avg<=51:
+    print("your grade  B")
+elif avg>=31 and avg<=41:
+    print("your grade  C+")
+elif avg>=21 and avg<=31:
+    print("your grade  C")
+elif avg>=11 and avg<=21:
+    print("your grade  C")
+elif avg>=6 and avg<=11:
+    print("your grade  D+")
+elif avg>=0 and avg<=6:
+    print("your grade  D")
 else:
-    print("You are severely obese.")
+    print(" 404!! ERROR INVAILD INPUT PAGE NOT FOUND")
+    
+
